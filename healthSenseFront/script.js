945123cf0da7396f61c80762b0ba6a626f523f50
Roxi <roxiroxi135@yahoo.com>
2024-05-12 13:48:04 +0300

    Am adaugat fornt-end.Afisare pacienti + login singup

---
+++ healthSenseFront/script.js
@@ -0,0 +1,167 @@
+var currentFilename = window.location.pathname.split('/').pop();
+$('ul li a').each(function() {
+    var href = $(this).attr('href').split('/').pop();
+    if (href === currentFilename) {
+        $(this).addClass('active');
+    }
+});
+
+/*****log-in or sing-up******/
+$(document).ready(function(){
+    $('.log').click(function(){
+        $('#login-form').show();
+        $('#signup-form').hide();
+        $('.reg').hide();
+        $('.log').hide();
+    });
+
+    $('.reg').click(function(){
+        $('#login-form').hide();
+        $('#signup-form').show();
+        $('.log').hide();
+        $('.reg').hide();
+    });
+
+    $('#login-form').submit(function(event){
+        event.preventDefault(); 
+
+        let formData = {
+            user: $('#login-email').val(),
+            password: $('#login-password').val()
+        };
+        console.log(formData);
+        fetch('http://localhost:8000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+            })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Response received:', data);
+            if(data?.status === 'Doctor') {
+                window.location = './doctor./acasa-doctor.html';
+            } else if(data?.status === 'Pacient') {
+                window.location = './pacient./acasa-pacient.html';
+            } else {
+                console.log('EROARE LA LOGARE');
+            }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    });
+
+    $('#signup-form').submit(function(event){
+        event.preventDefault();
+
+        var formData = {
+            user: $('#signup-email').val(),
+            password: $('#signup-password').val(),
+            status: $('#status').val() 
+        };
+        console.log(formData);
+
+        fetch('http://localhost:8000/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+            })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Response received:', data);
+            if(data?.status === 'Doctor') {
+                window.location('./acasa-doctor.html');
+            }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    });
+    $('#logoutButton').click(function() {
+        localStorage.removeItem("status");
+        window.location.href = "../log-in.html";
+    });
+
+});
+
+/**********Get patients*************/
+fetch('http://localhost:8000/pacienti', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+.then(responseData => {
+    // console.log('Response received:', responseData);
+    const data = responseData.data;
+    if (Array.isArray(data)) {
+        data.forEach(patient => {
+            const patientDiv = document.createElement('div');
+            patientDiv.classList.add('patient');
+            patientDiv.innerHTML = `
+                <p>Nume: ${patient.nume}</p>
+                <p>Prenume: ${patient.prenume}</p>
+                <p>Data nasterii: ${patient.dataNasterii}</p>
+                <p>Istoric al bolii: ${patient.istoricBolii}</p>
+                <p>Medicamentatie: ${patient.medicamentatie}</p>
+                <p>Adresa: ${patient.adresa}</p>
+                <p>Sex: ${patient.sex}</p>
+                <div class='btns'>
+                <button class='deleteBtn' data-id='${patient.id}' >Sterge</button>
+                <button class='UpdateBtn' data-id='${patient.id}' >Modifica pacient</button>
+                </div>
+            `;
+
+            const patientsContainer = document.getElementById('patients-container');
+            patientsContainer.appendChild(patientDiv);
+        });
+    } else {
+        console.error('Unexpected data format:', data);
+    }
+})
+.catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+});
+
+/*********add new patient************/
+document.getElementById('patient-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var formData = new FormData(this); 
+    fetch('http://localhost:8000/pacient', {
+        method: 'POST',
+        body: formData 
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Response received:', data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+});
+
